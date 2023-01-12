925cdcdd5d93738a214ab1af35faee955965761a
VakurinSergej <120665026+VakurinSergej@users.noreply.github.com>
2023-01-12 19:14:24 +0300

    Homework-5
    Only my homeworks on type 5

---
+++ Homework-5-3.cs
@@ -0,0 +1,40 @@
+﻿// Задача №1: Задайте массив вещественных чисел. 
+// Задача №2: Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    double number = new Random().Next(0, 10) + new Random().NextDouble(); 
+    array[i] = Math.Round(number, 2); 
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+double min = array[0];
+double max = array[0];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > max)
+        {
+            max = array[i];
+        }
+if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+}
+
+Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением: {max - min}");
+
+
+
+
+
+
